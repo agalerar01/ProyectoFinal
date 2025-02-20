bb757e4bf241f2d460b33af78bdcca6c55944daa
agalerar01 <alejandrogalerar@gmail.com>
2025-02-20 20:04:58 +0100

    hecho ajustes con sharedPreferences

--- app/src/main/java/com/example/proyectofinal/Main/Fragments/AparienciaFragment.java
+++ app/src/main/java/com/example/proyectofinal/Main/Fragments/AparienciaFragment.java
@@ -2,65 +2,85 @@ package com.example.proyectofinal.Main.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.proyectofinal.Main.SharedPreferencesHelper;
 import com.example.proyectofinal.R;
+import com.example.proyectofinal.databinding.FragmentAparienciaBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AparienciaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AparienciaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AparienciaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AparienciaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AparienciaFragment newInstance(String param1, String param2) {
-        AparienciaFragment fragment = new AparienciaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentAparienciaBinding binding;
+    SharedPreferencesHelper helper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_apariencia, container, false);
+
+        binding = FragmentAparienciaBinding.inflate(getLayoutInflater(), container, false);
+        helper = new SharedPreferencesHelper(getContext());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if(helper.devolverTemaOscuro()){
+            binding.switchModoOscuro.setChecked(true);
+        }else{
+            binding.switchModoOscuro.setChecked(false);
+        }
+
+        if(helper.devolverRecordatorio()){
+            binding.switchRecordador.setChecked(true);
+            binding.diasRecordatorio.setEnabled(true);
+            binding.diasRecordatorio.setText(String.valueOf(helper.devolverDiasRecordatorio()));
+        }else{
+            binding.switchRecordador.setChecked(false);
+            binding.diasRecordatorio.setEnabled(false);
+            binding.diasRecordatorio.setText("3");
+        }
+
+        binding.switchRecordador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!binding.switchRecordador.isChecked()){
+                    binding.diasRecordatorio.setEnabled(false);
+                }else{
+                    binding.diasRecordatorio.setEnabled(true);
+                }
+            }
+        });
+
+        binding.botonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.switchModoOscuro.setChecked(false);
+                binding.switchRecordador.setChecked(false);
+                binding.diasRecordatorio.setEnabled(false);
+                binding.diasRecordatorio.setText("3");
+
+                helper.guardarTemaOscuro(false);
+                helper.guardarRecordatorio(false);
+                helper.guardarDiasRecordatorio(3);
+            }
+        });
+
+        binding.guardarCambiosApariencia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helper.guardarTemaOscuro(binding.switchModoOscuro.isChecked());
+                helper.guardarRecordatorio(binding.switchRecordador.isChecked());
+                helper.guardarDiasRecordatorio(Integer.parseInt(String.valueOf(binding.diasRecordatorio.getText())));
+            }
+        });
     }
 }
