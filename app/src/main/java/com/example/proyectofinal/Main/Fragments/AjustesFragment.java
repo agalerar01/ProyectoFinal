48c64ccf00ac8b072e773b148c49cc8d1f6ba11e
agalerar01 <alejandrogalerar@gmail.com>
2025-02-20 17:51:37 +0100

    Puto navController de ajustes

--- app/src/main/java/com/example/proyectofinal/Main/Fragments/AjustesFragment.java
+++ app/src/main/java/com/example/proyectofinal/Main/Fragments/AjustesFragment.java
@@ -2,9 +2,16 @@ package com.example.proyectofinal.Main.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,5 +29,16 @@ public class AjustesFragment extends Fragment {
         binding = FragmentAjustesBinding.inflate(getLayoutInflater(), container, false);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getChildFragmentManager().findFragmentById(R.id.navHostFragmentAjustes);
+
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(binding.navigationAjustes, navController);
     }
 }
